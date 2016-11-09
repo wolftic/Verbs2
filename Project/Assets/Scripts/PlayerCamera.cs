6b134c918192fa079b9a77068e31b0025ed0ed41
wolftic <lorenzokramertje@gmail.com>
2016-11-09 16:36:02 +0100

    Camera null check

--- Project/Assets/Scripts/PlayerCamera.cs
+++ Project/Assets/Scripts/PlayerCamera.cs
@@ -8,6 +8,9 @@ public class PlayerCamera : MonoBehaviour {
 	private float _zoomLevel = 5, _maxZoom = 5f, _minZoom = 1f;
 	
 	void Update () {
+		if (target == null)
+			return;
+		
 		_zoomLevel += -Input.GetAxis ("Mouse ScrollWheel") * _zoomSpeed;
 		_zoomLevel = Mathf.Clamp (_zoomLevel, _minZoom, _maxZoom);
 		Vector3 pos = new Vector3 (0, _zoomLevel, -_zoomLevel);
