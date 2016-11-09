35ff06c0bf6f09ffd626dc0e77035c1278cc6286
wolftic <lorenzokramertje@gmail.com>
2016-11-09 14:31:01 +0100

    movement

--- Project/Assets/Scripts/Player.cs
+++ Project/Assets/Scripts/Player.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(NetworkPlayer))]
+[RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour {
+	private NetworkPlayer _networkPlayer;
+	private Rigidbody _rigidbody;
 
-	// Use this for initialization
+	[SerializeField]
+	private float _speed = 1f, _jumpForce = 100f;
+	private bool _onGround = true;
+
 	void Start () {
-	
+		_networkPlayer = GetComponent<NetworkPlayer> ();
+		_rigidbody = GetComponent<Rigidbody> ();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+		//_networkPlayer.pos = transform.position;
+
+		_onGround = Physics.Raycast (transform.position, Vector3.down, 1.1f);
+		Debug.DrawRay (transform.position, Vector3.down * 1.1f);
+
+		if (Input.GetButtonDown ("Jump") && _onGround) {
+			_rigidbody.AddForce(new Vector3(0, 100, 0), ForceMode.Impulse);
+		}
+	}
+
+	void FixedUpdate () {
+		Vector2 axis = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+		_rigidbody.velocity = new Vector3 (axis.x, _rigidbody.velocity.y, axis.y) * _speed;
 	}
 }
