33385d3559ea123f49b354c6c6276add2c06ea6c
wolftic <lorenzokramertje@gmail.com>
2016-11-11 17:50:52 +0100

    Final

---
+++ Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+	private Canvas canvas;
+
+	void Start () {
+		canvas = GetComponent<Canvas> ();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			canvas.enabled = !canvas.enabled;
+		}
+	}
+
+	public void QuitGame() {
+		Application.Quit ();
+	}
+}
